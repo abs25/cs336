dcd680e05258f0a082a1296d7d92560279eddd49
abs25 <abs25@students.calvin.edu>
2016-11-29 13:06:35 -0500

    add webpack

---
+++ app/scripts/WindowComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//WindowComponent
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+});
