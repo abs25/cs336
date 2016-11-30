31187b5fd6e4f902658c465e7f5383438febe8f3
abs25 <abs25@students.calvin.edu>
2016-11-30 10:36:09 -0500

    finish lab13

---
+++ app/scriptsbak/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Remarkable from 'remarkable';
+import $ from 'jquery';
+
+import CommentBox from './commentBox';
+import CommentEdit from './commentEdit'
+
+import '../css/base.css';
+
+import { Router, Route, Redirect, browserHistory } from 'react-router';
+
+import { StoreTools } from './flux';
+StoreTools.startLoadingComments();
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={CommentBox}/>
+    <Route path="/:id" component={CommentEdit} />
+  </Router>
+),
+  document.getElementById('content')
+);
