79beb0a8349e4755ffeb86b7a470fde03514b60e
abs25 <abs25@students.calvin.edu>
2016-12-01 08:06:56 -0500

    updating the scripts

---
+++ app/scripts/ScoreListLength.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/style.css';
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div className="scoreListLength">
+        <h3>10 - 50 - 100</h3>
+      </div>
+    );
+  }
+});
