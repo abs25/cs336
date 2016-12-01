f84538022b95160c31e4da00ee49d846964cc4f4
Kirby <codemonkey941@gmail.com>
2016-12-01 16:18:24 -0500

    Re-integrate working CSS.

--- app/scripts/ScoreList.js
+++ app/scripts/ScoreList.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import '../css/style.css';
+import styles from '../css/style.css';
 import Score from './Score';
 
 module.exports = React.createClass({
   render: function() {
     return (
-      <div className="scoreList">
+      <div className={styles.scoreList}>
         <h3>Rank - Name - Score - Date</h3>
         <Score />
       </div>
