15734b2bd1163339a3a11dfd9c8e62451a3f5ce0
Kirby <codemonkey941@gmail.com>
2016-11-30 20:44:58 -0500

    Add working CSS.

--- app/scripts/WindowComponent.js
+++ app/scripts/WindowComponent.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import AboutBox from './AboutBox';
 
+import '../css/style.css';
+
 //WindowComponent
 module.exports = React.createClass({
   render: function() {
     return (
       <div>
         <h1>Hello World</h1>
+        {this.props.children}
         <AboutBox />
       </div>
 
