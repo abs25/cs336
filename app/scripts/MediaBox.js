304cefcfa1ad856c4035aaadee7f7593986c59a9
abs25 <abs25@students.calvin.edu>
2016-12-01 08:06:33 -0500

    fixing merge conflict

--- app/scripts/MediaBox.js
+++ app/scripts/MediaBox.js
@@ -1,5 +1,11 @@
 import React from 'react';
-
-module.exports = React.createClass(
-
-);
+import '../css/style.css';
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div className="mediaBox">
+        <h2>Media</h2>
+      </div>
+    );
+  }
+});
--- app/scripts/Score.js
+++ app/scripts/Score.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import '../css/style.css';
-module.exports = React.createClass(
-
-);
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div className="score">
+        <ol>
+          <li>This is a score</li>
+        </ol>
+      </div>
+    );
+  }
+});
