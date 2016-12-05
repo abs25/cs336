57c8f0e82846e8ae78c020a035af665c1200afd8
Kirby <codemonkey941@gmail.com>
2016-12-05 13:21:10 -0500

    Add connection to the database.

--- project/server.js
+++ project/server.js
@@ -6,6 +6,17 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var MongoClient = require('mongodb').MongoClient;
+var databaseConnection;
+
+var url = 'mongodb://jeditrainer:' + process.env.PASSWORD + '@ds113628.mlab.com:13628/jeditrainerdb';
+
+MongoClient.connect(url, function(err, db) {
+	if (err) throw err;
+
+	databaseConnection = db;
+});
+
 var app = express();
 app.use('/', express.static(path.join(__dirname, 'dist')));
 
