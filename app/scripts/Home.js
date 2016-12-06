b9e50ca635d2e3a481fb48c42caf1c87e6c778ad
abs25 <abs25@students.calvin.edu>
2016-12-06 11:57:36 -0500

    add react router

---
+++ app/scripts/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from '../css/style.css'
+
+import AboutBox from './AboutBox';
+import ScoreBox from './ScoreBox';
+import MediaBox from './MediaBox';
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div className={styles.home}>
+        <AboutBox />
+        <ScoreBox />
+        <MediaBox />
+      </div>
+    )
+  }
+})
--- app/scripts/NavBar.js
+++ app/scripts/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../css/style.css';
+import { Link } from 'react-router'
 
 //this is the navigation bar
 module.exports = React.createClass({
@@ -7,6 +8,12 @@ module.exports = React.createClass({
     return (
       <div className={styles.navBar}>
         <h2>NavBar</h2>
+        {/* Create a list of links that correspond with the paths defined in index.js */}
+        <ul role="nav">
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/scores">Scores</Link></li>
+          <li><Link to="/media">Media</Link></li>
+        </ul>
       </div>
     );
   }
