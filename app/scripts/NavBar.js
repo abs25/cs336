21a3f2d7da871b0318177ff27cfeab5646eec264
abs25 <abs25@students.calvin.edu>
2016-12-06 12:13:31 -0500

    added IndexLink and turned on browserHistory

--- app/scripts/NavBar.js
+++ app/scripts/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../css/style.css';
-import { Link } from 'react-router'
+import { Link, IndexLink } from 'react-router'
 
 //this is the navigation bar
 module.exports = React.createClass({
@@ -10,9 +10,10 @@ module.exports = React.createClass({
         <h2>NavBar</h2>
         {/* Create a list of links that correspond with the paths defined in index.js */}
         <ul role="nav">
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/scores">Scores</Link></li>
-          <li><Link to="/media">Media</Link></li>
+          <li><IndexLink to="/" activeClassName={styles.active}>Home</IndexLink></li>
+          <li><Link to="/about" activeClassName={styles.active}>About</Link></li>
+          <li><Link to="/leaderboard" activeClassName={styles.active}>Leaderboard</Link></li>
+          <li><Link to="/media" activeClassName={styles.active}>Media</Link></li>
         </ul>
       </div>
     );
