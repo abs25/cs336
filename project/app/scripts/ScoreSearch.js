5e0fb1d5b6f55197b14ed6fd62d18d48fc36b846
Derek Dik <drd26@church.cs.calvin.edu>
2016-12-06 12:09:30 -0500

    Add search bar

--- project/app/scripts/ScoreSearch.js
+++ project/app/scripts/ScoreSearch.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import styles from '../css/style.css';
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {searchFilter: ''};
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var searchFilter = this.state.searchFilter.trim();
+    if (!searchFilter) {
+      return;
+    }
+    this.props.onSearchFilterSubmit(searchFilter);
+    this.setState({searchFilter: ''});
+  },
+  handleSearchFilterChange: function(e) {
+    this.setState({searchFilter: e.target.value});
+  },
   render: function() {
     return (
       <div className={styles.scoreSearch}>
-        <h3>Search</h3>
+      <form className="scoreSearchForm" onSubmit={this.handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search"
+        value={this.state.searchFilter}
+        onChange={this.handleSearchFilterChange}
+      />
+        <input type="submit" value="Search" />
+      </form>
       </div>
     );
   }
