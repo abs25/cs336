5005d2a70df968deb8b23574864d8ca78fca4b3c
Derek Dik <drd26@kernighan.cs.calvin.edu>
2016-12-07 13:48:54 -0500

    Search works

--- app/scripts/Score.js
+++ app/scripts/Score.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import styles from '../css/style.css';
 module.exports = React.createClass({
+      // TODO: Add other score fields
   render: function() {
     return (
       <div className={styles.score}>
-        <ol>
-          <li>This is a score</li>
-        </ol>
+        {this.props.name} - {this.props.scoreValue} - {this.props.date}
       </div>
     );
   }
