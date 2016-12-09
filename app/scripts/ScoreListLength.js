48a7358aff6280298d880e61a9d8f45164b919be
Derek Dik <drd26@hoare.cs.calvin.edu>
2016-12-09 15:01:56 -0500

    Added length filter

--- app/scripts/ScoreListLength.js
+++ app/scripts/ScoreListLength.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import styles from '../css/style.css';
+
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {lengthFilter: 10};
+  },
+  handleTen: function(){
+    this.handleLengthFilterChange(10);
+  },
+  handleFifty: function(){
+    this.handleLengthFilterChange(50);
+  },
+  handleOneHundred: function(){
+    this.handleLengthFilterChange(100);
+  },
+  handleLengthFilterChange: function(lengthFilter) {
+    this.setState({lengthFilter: lengthFilter});
+    this.props.onLengthFilterSubmit(lengthFilter);
+  },
   render: function() {
     return (
-      <div className={styles.scoreListLength}>
-        <h3>10 - 50 - 100</h3>
+      <div className={styles.aboutBox}>
+        <h3><button onClick={this.handleTen}>10</button> - <button onClick={this.handleFifty}>50</button> - <button onClick={this.handleOneHundred}>100</button> </h3>
       </div>
     );
   }
