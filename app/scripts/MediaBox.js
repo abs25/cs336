cae00cd45e173c3ad73105137c089ecde0eb89d5
Derek Dik <drd26@hoare.cs.calvin.edu>
2016-12-09 16:16:46 -0500

    Added video to Media tab

--- app/scripts/MediaBox.js
+++ app/scripts/MediaBox.js
@@ -6,6 +6,9 @@ module.exports = React.createClass({
     return (
       <div className={styles.mediaBox}>
         <h2>Media</h2>
+        <h3>Video</h3>
+        <iframe width="280" height="157" src="https://www.youtube.com/embed/8BpGmXSRCC4" frameborder="0" allowfullscreen></iframe>
+        <h3>Screenshots</h3>
       </div>
     );
   }
