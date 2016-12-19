5ef97f10784164cbab86f3b01a1d7e27c141f903
Kirby <codemonkey941@gmail.com>
2016-12-19 11:31:20 -0500

    Video player now has a wrapper.

--- project/app/scripts/MediaBox.js
+++ project/app/scripts/MediaBox.js
@@ -8,11 +8,11 @@ module.exports = React.createClass({
       <div className={styles.mediaBox}>
         <h2>Media</h2>
         <h3>Video</h3>
-        <ReactPlayer controls className={styles.responsiveVideo} url="https://youtu.be/8BpGmXSRCC4" />
+        <div className={styles.videoWrapper}><ReactPlayer width={250} height={200} controls className={styles.responsiveVideo} url="https://youtu.be/8BpGmXSRCC4" /></div>
         <h3>Screenshots</h3>
-        <div><img src={require('../img/Capture.png')} className={styles.responsiveImg} /></div>
-        <div><img src={require('../img/Capture2.png')} className={styles.responsiveImg} /></div>
-        <div><img src={require('../img/Capture3.png')} className={styles.responsiveImg} /></div>
+        <img src={require('../img/Capture.png')} className={styles.responsiveImg} />
+        <img src={require('../img/Capture2.png')} className={styles.responsiveImg} />
+        <img src={require('../img/Capture3.png')} className={styles.responsiveImg} />
       </div>
     );
   }
