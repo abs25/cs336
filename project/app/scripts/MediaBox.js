6e29d45f84ae40093ddf2fe3919b154df5e464db
abs25 <abs25@students.calvin.edu>
2016-12-19 12:09:50 -0500

    styled images

--- project/app/scripts/MediaBox.js
+++ project/app/scripts/MediaBox.js
@@ -7,17 +7,28 @@ module.exports = React.createClass({
     return (
       <div className={styles.mediaBox}>
         <h2>Media</h2>
-        <h3>Video</h3>
-        <div className={styles.videoWrapper}><ReactPlayer width={250} height={200} controls className={styles.responsiveVideo} url="https://youtu.be/8BpGmXSRCC4" /></div>
+        <div className={styles.images}>
         <h3>Screenshots</h3>
-        <h4>Our current project running on the Unreal engine</h4>
-        <img src={require('../img/Unreal.png')} className={styles.responsiveImg} />
-        <h4>An early WIP Jedi Trainer running on the Unity engine</h4>
-        <img src={require('../img/Unity.png')} className={styles.responsiveImg} />
-        <h4>Examples of blueprint code in Unreal</h4>
-        <img src={require('../img/CodeSnip1.png')} className={styles.responsiveImg} />
-        <img src={require('../img/CodeSnip2.png')} className={styles.responsiveImg} />
-        <img src={require('../img/CodeSnip3.png')} className={styles.responsiveImg} />
+          <div className={styles.unrealImages}>
+            <h5>Our current project running on the Unreal engine</h5>
+            <img src={require('../img/Unreal.png')} className={styles.responsiveImg} />
+          </div>
+          <div className={styles.unityImages}>
+            <h5>An early WIP Jedi Trainer running on the Unity engine</h5>
+            <img src={require('../img/Unity.png')} className={styles.responsiveImg} />
+          </div>
+          <div className={styles.codeImages}>
+            <h5>Examples of blueprint code in Unreal</h5>
+            <img src={require('../img/CodeSnip1.png')} className={styles.responsiveImg} />
+            <img src={require('../img/CodeSnip2.png')} className={styles.responsiveImg} />
+            <img src={require('../img/CodeSnip3.png')} className={styles.responsiveImg} />
+          </div>
+        </div>
+        <div className={styles.video}>
+          <h3>Video</h3>
+          <ReactPlayer width={280} height={174} controls className={styles.responsiveVideo} url="https://youtu.be/8BpGmXSRCC4" />
+        </div>
+
       </div>
     );
   }
