4e55c4a65218a76edc02d9d7f61a8fcca1cd70d7
Derek Dik <drd26@aiken.cs.calvin.edu>
2016-12-19 11:49:20 -0500

    Used images from presentation in MediaBox

--- project/app/scripts/MediaBox.js
+++ project/app/scripts/MediaBox.js
@@ -10,9 +10,14 @@ module.exports = React.createClass({
         <h3>Video</h3>
         <div className={styles.videoWrapper}><ReactPlayer width={250} height={200} controls className={styles.responsiveVideo} url="https://youtu.be/8BpGmXSRCC4" /></div>
         <h3>Screenshots</h3>
-        <img src={require('../img/Capture.png')} className={styles.responsiveImg} />
-        <img src={require('../img/Capture2.png')} className={styles.responsiveImg} />
-        <img src={require('../img/Capture3.png')} className={styles.responsiveImg} />
+        <h4>Our current project running on the Unreal engine</h4>
+        <img src={require('../img/Unreal.png')} className={styles.responsiveImg} />
+        <h4>An early WIP Jedi Trainer running on the Unity engine</h4>
+        <img src={require('../img/Unity.png')} className={styles.responsiveImg} />
+        <h4>Examples of blueprint code in Unreal</h4>
+        <img src={require('../img/CodeSnip1.png')} className={styles.responsiveImg} />
+        <img src={require('../img/CodeSnip2.png')} className={styles.responsiveImg} />
+        <img src={require('../img/CodeSnip3.png')} className={styles.responsiveImg} />
       </div>
     );
   }
